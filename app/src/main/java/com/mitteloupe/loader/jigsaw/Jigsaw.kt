cafb1f88e21147831b19d9fc80dfacb56a2b73ae
Eran Boudjnah <eranbou@gmail.com>
2024-03-11 12:54:24 +0000

    Updated dividers and icons. Minor code tidy up. (#38)

--- app/src/main/java/com/mitteloupe/loader/jigsaw/Jigsaw.kt
+++ app/src/main/java/com/mitteloupe/loader/jigsaw/Jigsaw.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -65,7 +65,7 @@ fun Jigsaw(navController: NavHostController) {
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
-                            imageVector = Icons.Filled.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Localized description"
                         )
                     }
