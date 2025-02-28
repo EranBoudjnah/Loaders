7a7b561e995abc9e179c4f9edd4976851d450e07
Eran Boudjnah <eranbou@gmail.com>
2025-02-28 14:11:59 +0000

    Bumped target SDK to 35. (#135)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.mitteloupe.loader"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mitteloupe.loader"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
